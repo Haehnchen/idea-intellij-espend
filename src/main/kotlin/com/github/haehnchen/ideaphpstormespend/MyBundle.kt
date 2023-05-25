6c8eb3f68674c317531a818cd545008d3db66684
GitHub Action <action@github.com>
2023-05-25 12:34:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/haehnchen/ideaphpstormespend/MyBundle.kt
+++ src/main/kotlin/com/github/haehnchen/ideaphpstormespend/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.haehnchen.ideaphpstormespend
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/haehnchen/ideaphpstormespend/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/haehnchen/ideaphpstormespend/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.haehnchen.ideaphpstormespend.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.haehnchen.ideaphpstormespend.MyBundle
+import com.github.haehnchen.ideaphpstormespend.services.MyProjectService
 import javax.swing.JButton
 
 
