f81d0f4f006b3fc1abc38bde7b50c1a40dea26b6
Daniel Espendiller <daniel@espendiller.net>
2023-05-29 13:05:53 +0200

    dummy tests can now be removed

--- src/main/java/de/espend/intellij/Dummy.java
+++
@@ -1,7 +0,0 @@
-package de.espend.intellij;
-
-public class Dummy {
-    boolean fun() {
-        return true;
-    }
-}
--- src/test/java/de/espend/intellij/DummyTest.java
+++
@@ -1,9 +0,0 @@
-package de.espend.intellij;
-
-import com.intellij.testFramework.fixtures.BasePlatformTestCase;
-
-public class DummyTest extends BasePlatformTestCase {
-    public void testDummy() {
-        assertTrue(new Dummy().fun());
-    }
-}
